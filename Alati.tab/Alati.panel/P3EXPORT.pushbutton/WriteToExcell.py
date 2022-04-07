4e2880af272e812d126a05dff432f4cc05556669
aleksastanojevic <stanojevic.aleksa93@gmail.com>
2022-04-07 16:50:33 +0200

    EXCEL-radi u VSC ali trepce

--- Alati.tab/Alati.panel/P3EXPORT.pushbutton/WriteToExcell.py
+++ Alati.tab/Alati.panel/P3EXPORT.pushbutton/WriteToExcell.py
@@ -2,20 +2,27 @@
 import clr
 import os
 
-clr.AddReference('Microsoft.Office.Interop.Excel')
+clr.AddReference('Microsoft.Office.Interop.Excel, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c')
 from Microsoft.Office.Interop import Excel
+from System.Runtime.InteropServices import Marshal
 
 lokacijaCuvanja=os.path.expanduser("~\\Desktop\\"+ 'Aleksa.xlsx')
 
-ex=Excel.ApplicationClass()
+with open(lokacijaCuvanja,'w',) as f:
+	print('NEW FILE')
+	
 
-ex.Visible=True
-# ex.DisplayAlerts = False
-xlbook = ex.Workbooks.Add()
 
-xlsheet= ex.Worksheets['PRIRUBNICE']
+excelApp=Excel.ApplicationClass()
+excelApp.Visible=True
+excelApp.DisplayAlerts = False
+
+xlbook = excelApp.Workbooks.Open('asdasd')
+# xlsheet=xlbook.Worksheets['Sheet1']
+ws=xlbook.Worksheets[1]
+
+xlsheet= excelApp.Worksheets['PRIRUBNICE']
 xlbook.ActiveSheet
 
 xlbook.SaveAs(lokacijaCuvanja)
-
-
+Marshal.ReleaseComObject(ws)
